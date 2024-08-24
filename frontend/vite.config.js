c755bf251763f955e69ad2889448a75bdcc13efe
Manish kumar <manishmandal9734@gmail.com>
2024-08-24 22:43:10 +0530

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: 'frontend/public', // Adjust this path if `index.html` is inside `frontend/public`
   plugins: [react()],
 })
-
-
-
