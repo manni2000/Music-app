de2af113c476873e615beba138e396161a39ee19
Manish kumar <manishmandal9734@gmail.com>
2024-08-24 22:33:51 +0530

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: 'frontend', // optional, only if your index.html is inside 'frontend' folder
+  root: 'frontend', // If your index.html is inside 'frontend' folder
   plugins: [react()],
-  build: {
-    outDir: 'dist', // or any other directory where you want your built files
-  },
 })
+
 
